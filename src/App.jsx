4fc89bad66e5f94f98bafe484c164610cc2ca897
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-17 15:31:39 +0200

    added to App.jsx all components..

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
+import { PageWelcome } from "./components/PageWelcome";
+import { PageRegister } from "./components/PageRegister";
+import { PageLogin } from "./components/PageLogin";
+import { NavLink, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "./AppContext";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  const { siteTitle } = useContext(AppContext);
+  return (
+    <div className="App">
+      <div className="App">
+        <h1>{siteTitle}</h1>
+        <hr />
+        <NavLink to="/welcome">Welcome</NavLink> |{" "}
+        <NavLink to="/register">Register</NavLink> |{" "}
+        <NavLink to="/login">Login</NavLink>
+        <hr />
+        <Routes>
+          <Route path="/welcome" element={<PageWelcome />} />
+          <Route path="/register" element={<PageRegister />} />
+          <Route path="/login" element={<PageLogin />} />
+          <Route path="/" element={<Navigate to="/welcome" replace />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
