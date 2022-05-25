5b992ea48ab9d89a2627743c7d0e35c76e60c408
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-26 00:27:21 +0200

    siteTitle changed

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const siteTitle = "Site Menu with React Router V6 and UseContext";
+  const siteTitle = "Site Menu React Vite UseContext";
   const [siteStatus, setSiteStatus] = useState("development");
 
   const toggleStatus = () => {
